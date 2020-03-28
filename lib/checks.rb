c966b7e0f22b43016fd1a198148fb222793bb1fa
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-03-28 07:46:46 +0100

    create check_indent mehtod

---
+++ lib/checks.rb
@@ -0,0 +1,14 @@
+module Checks
+  def check_indent(content_string, case_start, case_end)
+    space_indent = space_indent_check(content_string, case_start, case_end)
+    content_string.each_with_index do |elem, idx|
+      elem.reset
+      idx.scan(/\s+/)
+      string_pos = if elem.matched?
+        elem.matched.length
+      else
+        0
+      end
+    end
+  end
+end
